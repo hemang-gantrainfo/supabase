3c52f564066eb6234ccd8e46ff0f559f4dc5d19e
hemangzellis <hemang@zellis.com.au>
2026-03-17 16:03:59 +0530

    fixed test

--- api/test.js
+++ api/test.js
@@ -2,11 +2,7 @@ import { Client } from "pg";
 
 export default async function handler(req, res) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT || 6543,
-    database: process.env.POSTGRES_DATABASE,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+    connectionString: process.env.POSTGRES_URL,
     ssl: {
       rejectUnauthorized: false,
     },
@@ -14,13 +10,19 @@ export default async function handler(req, res) {
 
   try {
     await client.connect();
-    await client.query("SELECT 1");
+
+    const result = await client.query("SELECT 1");
+
     await client.end();
 
     return res.status(200).json({
       status: "✅ Connected to Supabase successfully!",
+      data: result.rows,
     });
+
   } catch (err) {
+    console.error("DB ERROR:", err);
+
     return res.status(500).json({
       status: "❌ Connection Failed",
       error: err.message,
